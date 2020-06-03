ca1e057a4f9fb1726ba18807ac980dbcead9443d
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-03 12:21:01 +0100

    added patch and delete basic routes and tested via postman

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -4,29 +4,43 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'handling GET requests to /products'
-    })
-})
+    });
+});
 
 router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'handling POST requests to /products'
-    })
-})
+    });
+});
 
 //get info about 1 single product
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    console.log(id)
+    console.log(id);
     if (id === 'special') {
         res.status(200).json({
             message: 'You discovered the special ID',
             id: id
-        })
+        });
     } else {
         res.status(200).json({
             message: 'You passed an ID'
-        })
+        });
     }
-})
+});
+
+//patch 1 single product
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+
+//delete 1 single product
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
+    });
+});
 
 module.exports = router;
