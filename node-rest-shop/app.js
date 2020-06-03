342ea5f041d7108bd0e421eb034c128008d1ac3f
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-03 15:14:40 +0100

    added error handler for if none of the routes accessed, as well as general error handler for whenever we need to throw error in the rest of the application

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//middleware, logs each request
+// Middleware, logs each request
 const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products')
@@ -10,5 +10,21 @@ app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// If none of the about routes accessed, execute error json response
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
