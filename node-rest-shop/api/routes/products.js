cfe2b58ea32025b234e2e2a4bdc048743fa39239
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-03 12:11:43 +0100

    added a basic get route that takes a product id. Includes basic logic to test route

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -13,4 +13,20 @@ router.post('/', (req, res, next) => {
     })
 })
 
+//get info about 1 single product
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    console.log(id)
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+
 module.exports = router;
