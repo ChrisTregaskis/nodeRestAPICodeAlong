656cee4c4d4ed2d281402ba0a8b3294c6f3e0ec8
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-05 21:01:25 +0100

    updated the response for get all orders

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -7,9 +7,24 @@ const Order = require('../models/order');
 router.get('/', (req, res, next) => {
     Order
         .find()
+        .select('product quantity _id')
         .exec()
         .then(docs => {
-            res.status(200).json(docs);
+            res.status(200).json({
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:5050/orders/' + doc._id
+                        }
+                    }
+                })
+
+            });
         })
         .catch(err => {
             res.status(500).json({
