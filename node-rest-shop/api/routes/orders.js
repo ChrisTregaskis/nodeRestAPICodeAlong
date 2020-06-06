2553e72f8b0190d782b892594a814df3038818e5
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-06 08:37:38 +0100

    updated DELETE order by id route and handled if order does not exist on GET order by id

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -77,6 +77,13 @@ router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
         .exec()
         .then(order => {
+
+            if (!order) {
+                return res.status(404).json({
+                    message: 'Order does not exist'
+                })
+            }
+
             res.status(200).json({
                 order: order,
                 request: {
@@ -93,10 +100,23 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
+    Order.deleteOne({_id: req.params.orderId})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Order deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:5050/orders/',
+                    body: { productId: '_id', quantity: 'Number'}
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
