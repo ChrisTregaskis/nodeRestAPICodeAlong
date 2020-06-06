54126ad5e70585fc1cbee510cfab5dc9b197b006
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-06 08:20:36 +0100

    updated orders post route, added logic if productId does not exist

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/order');
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
     Order
@@ -34,24 +35,42 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        product: req.body.productId,
-        quantity: req.body.quantity
-    });
+    Product.findById(req.body.productId)
+        .then(product => {
 
-    order
-        .save()
+            if (!product) {
+                return res.status(404).json({
+                    message: 'Product not found'
+                });
+            }
+
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                product: req.body.productId,
+                quantity: req.body.quantity
+            });
+
+            return order.save()
+        })
         .then(result => {
-            res.status(201).json(result)
+            res.status(201).json({
+                message: 'Order successfully placed',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:5050/orders/' + result._id
+                }
+            })
         })
         .catch(err => {
-            console.log(err)
             res.status(500).json({
                 error: err
-            })
+            });
         });
-
 });
 
 router.get('/:orderId', (req, res, next) => {
