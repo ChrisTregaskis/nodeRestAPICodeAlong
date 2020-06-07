ab990788296c218b36312c8711690dca3f99b81b
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-07 10:19:52 +0100

    added filter to make sure uploads are images and file size is below 5 megabites

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -12,7 +12,22 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage: storage})
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 *1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require('../models/product')
 
