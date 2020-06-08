3396ad9f6db17d8f33f82b91f065d1d27be87de0
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-08 21:16:00 +0100

    added checkAuth on ALL routes BAR GET

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -132,7 +132,7 @@ router.get('/:productId', (req, res, next) => {
         });
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOperations = {};
 
@@ -160,7 +160,7 @@ router.patch('/:productId', (req, res, next) => {
 
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product
         .deleteOne({_id: id})
