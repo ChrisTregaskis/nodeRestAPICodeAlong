4c1255a7ba3fd2b50cfb80bd4a4cba08fbe58ef2
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-08 16:30:20 +0100

    created login route, added check of result length, comparing req pass with pass in db, managing err and response result

--- node-rest-shop/api/routes/user.js
+++ node-rest-shop/api/routes/user.js
@@ -45,6 +45,43 @@ router.post('/signup', (req, res, next) => {
         })
 });
 
+router.post('/login', (req, res, next) => {
+   User.find({ email: req.body.email }) // could use findOne to not return type: array
+       .exec()
+       .then(user => {
+
+           if (user.length < 1) {
+               return res.status(401).json({
+                   message: 'Auth failed'
+               });
+           }
+
+           bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+
+               if (err) {
+                   return res.status(401).json({
+                       message: 'Auth failed'
+                   });
+               }
+
+               if (result) {
+                   return res.status(200).json({
+                       message: 'Auth successful'
+                   });
+               }
+
+               res.status(401).json({
+                   message: 'Auth failed'
+               });
+           })
+       })
+       .catch(err => {
+           res.status(500).json({
+               error: err
+           })
+       });
+});
+
 router.delete('/:userId', (req, res, next) => {
     User.deleteOne({_id: req.params.userId})
         .exec()
