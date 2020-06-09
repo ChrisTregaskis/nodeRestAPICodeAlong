6f5d9feed65eb38c9c8bc70322bb77c0c3d2481e
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-09 10:41:03 +0100

    refactored delete route and moved logic to orders controller

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -9,29 +9,8 @@ const Product = require('../models/product');
 const OrdersController = require('../controllers/orders');
 
 router.get('/', checkAuth, OrdersController.orders_get_all);
-
 router.post('/', checkAuth, OrdersController.orders_create_new);
-
 router.get('/:orderId', checkAuth, OrdersController.orders_get_single);
-
-router.delete('/:orderId', checkAuth, (req, res, next) => {
-    Order.deleteOne({_id: req.params.orderId})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'Order deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:5050/orders/',
-                    body: { productId: '_id', quantity: 'Number'}
-                }
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.delete('/:orderId', checkAuth, OrdersController.orders_delete_order);
 
 module.exports = router;
