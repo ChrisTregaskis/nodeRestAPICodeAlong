61a6f0d672edf425c7d92a4abd6f93df6e3e33a4
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-09 10:34:28 +0100

    added and exporting orders_create_new method, required in Product model and mongoose

--- node-rest-shop/api/controllers/orders.js
+++ node-rest-shop/api/controllers/orders.js
@@ -1,4 +1,6 @@
+const mongoose = require('mongoose');
 const Order = require('../models/order');
+const Product = require('../models/product');
 
 exports.orders_get_all = (req, res, next) => {
     Order
@@ -28,4 +30,43 @@ exports.orders_get_all = (req, res, next) => {
                 error: err
             })
         });
-}
+};
+
+exports.orders_create_new = (req, res, next) => {
+    Product.findById(req.body.productId)
+        .then(product => {
+
+            if (!product) {
+                return res.status(404).json({
+                    message: 'Product not found'
+                });
+            }
+
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                product: req.body.productId,
+                quantity: req.body.quantity
+            });
+
+            return order.save()
+        })
+        .then(result => {
+            res.status(201).json({
+                message: 'Order successfully placed',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:5050/orders/' + result._id
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+};
